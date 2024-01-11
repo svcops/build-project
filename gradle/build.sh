14833b745e32736efc9297266cd4b0964412ed45
zhenjie zhu <devops@kubectl.net>
2024-01-11 20:33:59 +0800

    Add docker support and image, cache, command for Gradle build script
    The commit expands the Gradle build script by adding Docker support and integrating command-line options to specify cache, image, and build commands. If Docker isn't installed, it'll give a proper notification and exit the script. It also puts validation in place for important build parameters to make sure they are not empty.

--- gradle/build.sh
+++ gradle/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2086
 
 function log() {
   local log_remark="$1"
@@ -15,6 +16,89 @@ function log() {
   echo -e "$current_time - [ $log_remark ] $log_message"
 }
 
+command_exists() {
+  # this should return the exit status of 'command -v'
+  command -v "$1" >/dev/null 2>&1
+}
+
 log "gradle build" ">>> start <<<"
+
+# 构建的缓存
+cache=""
+# 构建的镜像
+image=""
+# 构建的命令
+build=""
+
+function tips() {
+  log "tips" "-c cache"
+  log "tips" "-i image"
+  log "tips" "-x execute"
+}
+
+while getopts ":c:i:x:" opt; do
+  case ${opt} in
+  c)
+    log "getopts" "构建的缓存 docker's volume 为: $OPTARG"
+    cache=$OPTARG
+    ;;
+  i)
+    log "getopts" "构建的镜像 docker's image 为: $OPTARG"
+    image=$OPTARG
+    ;;
+  x)
+    log "getopts" "构建的命令 gradle's command 为: $OPTARG"
+    build=$OPTARG
+    ;;
+  \?)
+    log "getopts" "Invalid option: -$OPTARG"
+    tips
+    log "gradle build" ">>> end <<<"
+    exit 1
+    ;;
+  :)
+    log "getopts" "Invalid option: -$OPTARG requires an argument"
+    tips
+    log "gradle build" ">>> end <<<"
+    exit 1
+    ;;
+  esac
+done
+
+function validate_param() {
+  local key=$1
+  local value=$2
+  if [ -z "$value" ]; then
+    log "validate_param" "构建的参数 $key 为空, 退出"
+    tips
+    log "gradle build" ">>> end <<<"
+    exit 1
+  else
+    log "validate_param" "构建的参数 $key : $value"
+  fi
+}
+
+validate_param "cache" "$cache"
+validate_param "image" "$image"
+validate_param "build" "$build"
+
+if command_exists docker; then
+  log "command_exists" "docker 命令存在"
+  exit
+else
+  log "command_exists" "docker 命令不存在"
+  log "gradle build" ">>> end <<<"
+  exit 1
+fi
+
+log "build" "========== gradle =========="
+
+docker run --rm -u root \
+  --network=host \
+  -v "$PWD":/home/gradle/project \
+  -w /home/gradle/project \
+  -v "$cache:/home/gradle/.gradle" \
+  "$image" \
+  $build
 
 log "gradle build" ">>> end <<<"
