58c35e8098a7582e41a00bbe35cf335adefa097b
zhenjie zhu <devops@kubectl.net>
2024-01-17 17:20:23 +0800

    Enhance docker rmi functionality with removal strategies
    The docker rmi functionality has been enhanced to include user-specified removal strategies as a new parameter. The script now accepts an image name and a removal strategy, thus enabling more flexible management of Docker images. It now also includes error checking on passed parameters and logs actions for improved troubleshooting.

--- docker/rmi.sh
+++ docker/rmi.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# shellcheck disable=SC2086
+# shellcheck disable=SC2046
+# shellcheck disable=SC2126
 
 function log() {
   local log_remark="$1"
@@ -15,10 +18,70 @@ function log() {
   echo -e "$current_time - [ $log_remark ] $log_message"
 }
 
+log "docker rmi" ">>> docker rmi start <<<"
+function end() {
+  log "docker rmi" ">>> docker rmi end <<<"
+}
+
 command_exists() {
   # this should return the exit status of 'command -v'
   command -v "$1" >/dev/null 2>&1
 }
+
+function tips() {
+  log "tips" "-i image name"
+  log "tips" "-s strategy: contain_latest remove_none all"
+}
+
+title_reg="^REPOSITORY\s*TAG\s*IMAGE\s*\ID\s*CREATED\s*SIZE$"
+
+image_name=""
+strategy=""
+
+while getopts ":i:s:" opt; do
+  case ${opt} in
+  i)
+    log "getopts" "image name is : $OPTARG"
+    image_name=$OPTARG
+    ;;
+  s)
+    log "getopts" "remove strategy is : $OPTARG"
+    strategy=$OPTARG
+    ;;
+  \?)
+    log "getopts" "Invalid option: -$OPTARG"
+    tips
+    end
+    exit 1
+    ;;
+  :)
+    log "getopts" "Invalid option: -$OPTARG requires an argument"
+    tips
+    end
+    exit 1
+    ;;
+  esac
+done
+
+function validate_param() {
+  local key=$1
+  local value=$2
+  if [ -z "$value" ]; then
+    log "validate_param" "parameter $key is empty, then exit"
+    tips
+    end
+    exit 1
+  else
+    log "validate_param" "parameter $key : $value"
+  fi
+}
+
+validate_param "image_name" "$image_name"
+
+if [ -z "$strategy" ]; then
+  log "strategy" "strategy is empty use default contain_latest"
+  strategy="contain_latest"
+fi
 
 if command_exists docker; then
   log "command_exists" "docker 命令存在"
@@ -28,4 +91,41 @@ else
   exit 1
 fi
 
+if [ "$strategy" == "contain_latest" ]; then
 
+  con=$(docker docker image ls $image_name | grep -v $title_reg | grep -v "latest" | wc -l)
+
+  if [ $con -eq 0 ]; then
+    log "contain_latest" "image doesn't exit ,then exit"
+    end
+    exit
+  fi
+
+  docker image rm -f $(docker docker image ls $image_name | grep -v $title_reg | grep -v "latest" | awk '{print $3}')
+
+elif [ "$strategy" == "remove_none" ]; then
+
+  con=$(docker docker image ls $image_name | grep -v $title_reg | grep "<none>" | wc -l)
+  if [ $con -eq 0 ]; then
+    log "remove_none" "image doesn't exit ,then exit"
+    end
+    exit
+  fi
+
+  docker image rm -f $(docker docker image ls $image_name | grep -v $title_reg | grep "<none>" | awk '{print $3}')
+
+elif [ "$strategy" == "all" ]; then
+
+  con=$(docker docker image ls $image_name | grep -v $title_reg | wc -l)
+
+  if [ $con -eq 0 ]; then
+    log "all" "image doesn't exit ,then exit"
+    end
+    exit
+  fi
+
+  docker image rm -f $(docker docker image ls $image_name | grep -v $title_reg | awk '{print $3}')
+
+fi
+
+end
