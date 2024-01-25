bedb70bd713d710fe6d530979eb5783a20db460a
zhenjie zhu <devops@kubectl.net>
2024-01-25 16:45:25 +0800

    Update default log identifiers in log.sh
    The default identifiers of 'log_remark' and 'log_message' have been updated in the log.sh script. Previously, any empty log remarks would default to "default remark", and empty log messages would default to "default message". Now, they will display as "unknown remark" and "unknown message".

--- func/log.sh
+++ func/log.sh
@@ -3,10 +3,10 @@ function log() {
   local log_remark="$1"
   local log_message="$2"
   if [ -z "$log_remark" ]; then
-    log_remark="default remark"
+    log_remark="unknown remark"
   fi
   if [ -z "$log_message" ]; then
-    log_message="default message"
+    log_message="unknown message"
   fi
   local current_time=$(date +"%Y-%m-%d %H:%M:%S")
   echo -e "$current_time - [ $log_remark ] $log_message"
