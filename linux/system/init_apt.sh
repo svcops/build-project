eddd6770aa299b3720a376f0631177fb78460739
zhenjie zhu <devops@kubectl.net>
2024-03-04 22:18:49 +0800

    Switch from apt to apt-get in init_apt.sh
    The changes made replace the use of 'apt' commands with 'apt-get' commands in the 'init_apt.sh' file. This is a more robust option for scripting since 'apt-get' provides more stable CLI between versions, ensuring long-term compatibility.

--- linux/system/init_apt.sh
+++ linux/system/init_apt.sh
@@ -5,9 +5,9 @@ source <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main
 
 log "system os" "os is $(bash <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main/linux/system/detect_os.sh))"
 
-apt update -y
+apt-get update -y
 
 export DEBIAN_FRONTEND=noninteractive
-apt-o Dpkg::Options::=--force-confold -o Dpkg::Options::=--force-confdef -y --allow-downgrades --allow-remove-essential --allow-change-held-packages
+apt-get-o Dpkg::Options::=--force-confold -o Dpkg::Options::=--force-confdef -y --allow-downgrades --allow-remove-essential --allow-change-held-packages
 
-apt install -y sudo vim git wget net-tools jq
+apt-get install -y sudo vim git wget net-tools jq
