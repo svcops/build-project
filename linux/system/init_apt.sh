371a300dcb0dc7be0b73f42a5043412b093a49e3
zhenjie zhu <devops@kubectl.net>
2024-03-04 22:02:28 +0800

    Refine non-interactive APT upgrade command
    This commit modifies the APT upgrade command in the init_apt.sh script. The modification uses DEBIAN_FRONTEND=noninteractive to suppress prompts and includes options to allow downgrades, remove essential packages, and change held packages. This offers a smoother, non-interrupted upgrade process.

--- linux/system/init_apt.sh
+++ linux/system/init_apt.sh
@@ -6,6 +6,11 @@ source <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main
 log "system os" "os is $(bash <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main/linux/system/detect_os.sh))"
 
 apt update -y
-apt-get upgrade -y -o Dpkg::Options::="--force-confold" --allow
+
+DEBIAN_FRONTEND=noninteractive \
+  apt-get \
+  -o Dpkg::Options::=--force-confold \
+  -o Dpkg::Options::=--force-confdef \
+  -y --allow-downgrades --allow-remove-essential --allow-change-held-packages
 
 apt install -y sudo vim git wget net-tools jq
