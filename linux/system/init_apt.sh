d5f256d9032b4360001b650adb706d110b238803
zhenjie zhu <devops@kubectl.net>
2024-03-04 21:56:16 +0800

    Update the apt-get upgrade command options
    The previously used `--force-confdef` option in the apt-get upgrade command has been removed. Now, only the `--force-confold` option is used. This should ensure that old config files are retained during the upgrade, as per the force-confold setting.

--- linux/system/init_apt.sh
+++ linux/system/init_apt.sh
@@ -6,6 +6,6 @@ source <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main
 log "system os" "os is $(bash <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main/linux/system/detect_os.sh))"
 
 apt update -y
-apt-get upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --allow
+apt-get upgrade -y -o Dpkg::Options::="--force-confold" --allow
 
 apt install -y sudo vim git wget net-tools jq
