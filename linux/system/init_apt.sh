eff6d80ad4d23c5ecc31a1eca331eae35022ecf0
zhenjie zhu <devops@kubectl.net>
2024-03-04 14:04:46 +0800

    Update init_apt.sh with log functionality and OS detection
    Added a logging function and operating system detection to the initialization process. The updated script now sources the log.sh script and runs OS detection before performing system updates and upgrades. The curl tool installation was specifically included to support these features.

--- linux/system/init_apt.sh
+++ linux/system/init_apt.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # debian 系列系统初始化
+# shellcheck disable=SC1090
+source <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main/func/log.sh)
+
+apt install -y curl
+
+log "system os" "os is $(bash <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main/linux/system/detect_os.sh))"
 
 apt update -y
 apt upgrade -y
 
-apt install -y sudo vim git curl wget net-tools
+apt install -y sudo vim git wget net-tools
