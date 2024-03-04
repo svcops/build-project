feb1a7b7417a875cc7a4844a96ea07155b5f1ad7
zhenjie zhu <devops@kubectl.net>
2024-03-04 13:40:42 +0800

    Add UFW installation and configuration functions
    In the 'install_apt.sh' script, functions for installing Uncomplicated Firewall (UFW), enabling it, allowing SSH through it, and providing usage tips have been added. This offers two installation strategies: 'oi' for only installation, and 'oics' for installation and SSH configuration.

--- linux/ufw/install_apt.sh
+++ linux/ufw/install_apt.sh
@@ -12,10 +12,44 @@ function detect_ssh_port() {
 
 detect_ssh_port
 
+function install_ufw() {
+  apt install -y ufw
+}
+
+function enable_ufw() {
+  log "ufw" "enable_ufw"
+  systemctl enable ufw
+  ufw enable
+  ufw status
+}
+
+function ufw_allow_ssh() {
+  log "ufw" "ufw_allow_ssh"
+  ufw allow $ssh_port
+  ufw reload
+  ufw status
+}
+
+function tips() {
+  log "tips" "install strategy: oi (only install and do not config), oics (install and config ssh)"
+  log "tips" "e.g.: bash <(curl -SL https://code.kubectl.net/devops/build-project/raw/branch/main/linux/ufw/install_apt.sh) oi"
+  log "tips" "e.g.: bash <(curl -SL https://code.kubectl.net/devops/build-project/raw/branch/main/linux/ufw/install_apt.sh) oics"
+}
+
 if command_exists ufw; then
   echo "ufw is installed."
-  # todo
 else
   echo "ufw is NOT installed."
-  # todo
+fi
+
+strategy=$1
+
+if [ "$strategy" == "oi" ]; then
+  install_ufw
+elif [ "$strategy" == "oics" ]; then
+  install_ufw
+  enable_ufw
+  ufw_allow_ssh
+else
+  tips
 fi
