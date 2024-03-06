b8cf2b9d6628600451c768374acd274200865ddc
zhenjie zhu <devops@kubectl.net>
2024-03-06 13:10:19 +0800

    Update timestamp format and improve script logging
    The timestamp format in maven/config.sh has been updated to replace spaces with underscores for compatibility reasons. Additionally, the status of settings files is now logged for better troubleshooting and verification of script actions.

--- maven/config.sh
+++ maven/config.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC1090
 source <(curl -SL https://code.kubectl.net/devops/build-project/raw/branch/main/func/log.sh)
 
-now=$(date +"%Y-%m-%d %H:%M:%S")
+now=$(date +"%Y-%m-%d_%H-%M-%S")
 
 log "create folder" "try create m2 folder"
 mkdir -p "$HOME/.m2"
@@ -19,6 +19,7 @@ backup_settings
 function download_settings() {
   curl -SL https://code.kubectl.net/devops/build-project/raw/branch/main/maven/settings.xml -o "$HOME/.m2/settings.xml"
   log "show settings" "ls -l $HOME/.m2/"
+  ls -l "$HOME/.m2/"
 }
 
 download_settings
