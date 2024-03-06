ff40a001621a53711ab72679d97b80fce867e632
zhenjie zhu <devops@kubectl.net>
2024-03-06 13:52:25 +0800

    Update keyring and list names in Docker install script
    The keyring and list file names in the Docker installation script for the Aliyun package repository have been updated. This change simplifies their naming scheme without altering their functionality.

--- docker/install/ubuntu/install_aliyun.sh
+++ docker/install/ubuntu/install_aliyun.sh
@@ -9,14 +9,14 @@ log "install" "install docker"
 sudo apt-get update -y
 sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker_ubuntu_aliyun.gpg
+curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker_ubuntu_aliyun.gpg] https://mirrors.aliyun.com/docker-ce/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.aliyun.com/docker-ce/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-  sudo tee /etc/apt/sources.list.d/docker_ubuntu_aliyun.list >/dev/null
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 log "install" "apt-get install ..."
 sudo apt-get update -y
