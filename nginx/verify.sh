00c3759c1c47449dc73824d64e3b47279496c4eb
zhenjie zhu <devops@kubectl.net>
2024-04-02 02:21:36 +0800

    Expand verify_nginx_configuration function parameters
    The parameters of the function verify_nginx_configuration in the verify.sh file have been expanded. Previously, this function only accepted a single input. Now, it's capable of taking two arguments, enhancing its functionality and scope.

--- nginx/verify.sh
+++ nginx/verify.sh
@@ -2,4 +2,4 @@
 # shellcheck disable=SC1090
 source <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main/nginx/verify_func.sh)
 
-verify_nginx_configuration "$1"
+verify_nginx_configuration "$1" "$2"
