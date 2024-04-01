9dfba93918399f884c8b60b4dc0d99e25c8c14e6
zhenjie zhu <devops@kubectl.net>
2024-04-02 03:04:01 +0800

    Expand verify.sh with additional checks and logs
    In the nginx/verify.sh script, the single source link has been replaced with two different source links for logging and checking command existence. Furthermore, the script now includes checks for whether needed files and parameters are provided and warns the user if not. It now also outputs appropriate logs in case of verification failure or success.

--- nginx/verify.sh
+++ nginx/verify.sh
@@ -1,9 +1,66 @@
 #!/bin/bash
-# shellcheck disable=SC1090
-source <(curl -SL https://code.kubectl.net/devops/build-project/raw/branch/main/nginx/verify_func.sh)
+# shellcheck disable=SC1090 disable=SC2164
+source <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main/func/log.sh)
+source <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main/func/command_exists.sh)
 
-function verify() {
-  verify_nginx_configuration "$1" "$2"
-}
+log "nginx" "Verify the nginx configuration file that docker-compose starts"
+compose_file=$2
+service_name=$1
 
-verify "$1" "$2"
+if [ -z "$compose_file" ]; then
+  log "nginx" "compose file is empty, try use docker-compose.yml or docker-compose.yaml"
+  if [ -f "docker-compose.yml" ]; then
+    compose_file="docker-compose.yml"
+  elif [ -f "docker-compose.yaml" ]; then
+    compose_file="docker-compose.yaml"
+  else
+    log "nginx" "cannot find docker-compose.yml or docker-compose.yaml in current directory"
+    exit 1
+  fi
+elif [ ! -f "$compose_file" ]; then
+  log "nginx" "compose file does not exits"
+  exit 1
+fi
+
+COMPOSE_FILE_FOLDER=$(cd "$(dirname "$compose_file")" && pwd)
+log "nginx" "compose file dir is $COMPOSE_FILE_FOLDER"
+
+if [ -z "$service_name" ]; then
+  log "nginx" "service_name is empty,[compose_file=$compose_file,service_name=$service_name] then exit 1"
+  exit 1
+fi
+
+if command_exists docker-compose; then
+  log "nginx" "use docker-compose"
+  output=$(docker-compose -f "$compose_file" run --rm -it "$service_name" nginx -t | tail -n 2 | grep 'nginx:')
+else
+  log "nginx" "use docker compose plugin"
+  output=$(docker compose -f "$compose_file" run --rm -it "$service_name" nginx -t | tail -n 2 | grep 'nginx:')
+fi
+
+if [ -z "$output" ]; then
+  log "nginx" "output is empty. Unknown Configuration, [compose_file=$compose_file,service_name=$service_name] then exit 1"
+  exit 1
+fi
+
+# template
+
+#nginx: the configuration file /etc/nginx/nginx.conf syntax is ok
+#nginx: configuration file /etc/nginx/nginx.conf test is successful
+
+#nginx: [emerg] unknown directive "xworker_processes" in /etc/nginx/nginx.conf:1
+#nginx: configuration file /etc/nginx/nginx.conf test failed
+
+reason=$(echo "$output" | sed -n '1p')
+status=$(echo "$output" | sed -n '2p')
+
+if echo "$status" | grep -q "successful"; then
+  log "nginx" "$status"
+  return 0
+elif echo "$status" | grep -q "failed"; then
+  log "nginx" "$reason"
+  exit 1
+else
+  log "nginx" "Unknown Configuration, [compose_file=$compose_file,service_name=$service_name] then exit 1"
+  exit 1
+fi
