41dca870dcc807036343db33f3c90692688d6a10
zhenjie zhu <devops@kubectl.net>
2024-04-02 03:11:05 +0800

    Add logging for nginx validation command
    The commit adds detailed logging in the nginx verification scripts for both `docker-compose` and `docker compose` commands. This will help in debugging by providing specific logs about the nginx command being executed under different conditions.

--- nginx/verify.sh
+++ nginx/verify.sh
@@ -32,9 +32,11 @@ fi
 
 if command_exists docker-compose; then
   log "nginx" "use docker-compose"
+  log "nginx" "docker-compose -f $compose_file run --rm -it $service_name nginx -t | tail -n 2 | grep 'nginx:'"
   output=$(docker-compose -f "$compose_file" run --rm -it "$service_name" nginx -t | tail -n 2 | grep 'nginx:')
 else
   log "nginx" "use docker compose plugin"
+  log "nginx" "docker compose -f $compose_file run --rm -it $service_name nginx -t | tail -n 2 | grep 'nginx:'"
   output=$(docker compose -f "$compose_file" run --rm -it "$service_name" nginx -t | tail -n 2 | grep 'nginx:')
 fi
 
