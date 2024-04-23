58c3c10fc1dbc589145abfa098b88e6ba306ac8e
zhenjie zhu <devops@kubectl.net>
2024-04-23 15:49:48 +0800

    Add build.sh script for writerside project
    The new script is designed to handle building of the writerside project. It includes input validation, logging functionalities, as well as parameterized building routine using a Docker image. The script also ensures that the necessary directories exist before proceeding with the build.

---
+++ writerside/build.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# shellcheck disable=SC1090
+source <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main/func/log.sh)
+source <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main/func/command_exists.sh)
+
+log "wsr build" ">>> writerside build start <<<"
+function end() {
+  log "wsr build" ">>> writerside build end <<<"
+}
+
+# writer side 构建的镜像
+build_image="registry.cn-shanghai.aliyuncs.com/iproute/wrs-builder:233.14938"
+
+function tips() {
+  log "tips" "-d writerside build directory"
+  log "tips" "-i writerside build instance"
+}
+
+build_dir=""
+instance=""
+
+while getopts ":d:i:" opt; do
+  case ${opt} in
+  d)
+    log "get opts" "build_dir is : $OPTARG"
+    build_dir=$OPTARG
+    ;;
+  i)
+    log "get opts" "instance is : $OPTARG"
+    instance=$OPTARG
+    ;;
+  \?)
+    log "get opts" "Invalid option: -$OPTARG"
+    tips
+    end
+    exit 1
+    ;;
+  :)
+    log "get opts" "Invalid option: -$OPTARG requires an argument"
+    tips
+    end
+    exit 1
+    ;;
+  esac
+done
+
+function validate_param() {
+  local key=$1
+  local value=$2
+  if [ -z "$value" ]; then
+    log "validate_param" "parameter $key is empty, then exit"
+    tips
+    end
+    exit 1
+  else
+    log "validate_param" "parameter $key : $value"
+  fi
+}
+
+validate_param "build_dir" "$build_dir"
+validate_param "instance" "$instance"
+
+if [ ! -d "$build_dir" ]; then
+  log "build_dir" "$build_dir does not exist,exit"
+  tips
+  end
+  exit 1
+fi
+
+if [ ! -d "$build_dir/Writerside" ]; then
+  log "Writerside" "Writerside dir does not exist,exit"
+  tips
+  end
+  exit 1
+fi
+
+if [ -d "$build_dir/output" ]; then
+  log "clear" "rm -rf output"
+  rm -rf output
+fi
+
+docker run --rm \
+  -v "$build_dir:/opt/sources" \
+  -e INSTANCE="Writerside/$instance" \
+  $build_image
