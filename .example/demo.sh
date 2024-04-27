ee6dee70643c6bb57f7bdd661a83d6bf23c9a7cb
zhenjie zhu <devops@kubectl.net>
2024-04-28 03:58:02 +0800

    Update log message in demo.sh script
    This commit changes the second argument of the log call in the demo.sh script. Specifically, it changes it from "mirror" to "demo" in order to match the name of the demo environment.

--- .example/demo.sh
+++ .example/demo.sh
@@ -6,4 +6,4 @@ cd "$SHELL_FOLDER"
 # shellcheck disable=SC1090
 source <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main/func/log.sh)
 
-log "demo" "mirror"
+log "demo" "demo"
