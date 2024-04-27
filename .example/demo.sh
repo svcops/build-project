df99b26b04865c8268ae5ee5853923054c6efd67
zhenjie zhu <devops@kubectl.net>
2024-04-28 03:59:21 +0800

    Remove demo.sh script
    The script demo.sh was deleted from the .example directory, as it is no longer needed. This cleanup is part of the ongoing effort to maintain a lean codebase.

--- .example/demo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# shellcheck disable=SC2164
-SHELL_FOLDER=$(cd "$(dirname "$0")" && pwd)
-cd "$SHELL_FOLDER"
-
-# shellcheck disable=SC1090
-source <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main/func/log.sh)
-
-log "demo" "demo"
