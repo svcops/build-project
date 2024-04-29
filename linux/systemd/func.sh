9ca69eb4944e0b12ff9923ef367b3cef375998c3
zhenjie zhu <devops@kubectl.net>
2024-04-29 17:37:47 +0800

    Add systemd service handler functions
    This commit introduces new bash functions to handle systemd services. The functions `stop_systemd_service` and `start_systemd_service` have been added to stop and start systemd services respectively, complete with error logging in case the service does not exist.

---
+++ linux/systemd/func.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# shellcheck disable=SC1090
+source <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main/func/log.sh)
+
+function stop_systemd_service() {
+  local service_name=$1
+  log "systemd" "stop systemd service: $service_name"
+  if systemctl -l | grep -q "$service_name"; then
+    systemctl stop "$service_name"
+    return 0
+  else
+    log "systemd" "service does not exist : $service_name"
+    return 1
+  fi
+}
+
+function start_systemd_service() {
+  local service_name=$1
+  log "systemd" "start systemd service: $service_name"
+  if systemctl -l -a | grep -q "$service_name"; then
+    systemctl restart "$service_name"
+    return 0
+  else
+    log "systemd" "service does not exist : $service_name"
+    return 1
+  fi
+}
