18d19f0db1d0cb03f219ad3b0963f797f1e8597e
zhenjie zhu <devops@kubectl.net>
2024-04-30 00:35:49 +0800

    Add date.sh script for various date formats
    The new bash script, date.sh, has been added to generate various date formats. This script will output four different formats of current date and time. These formats can be used for versioning or timestamp purposes.

---
+++ func/date.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+data_general=$(date '+%Y-%m-%d %H:%M:%S')
+datetime_version=$(date '+%Y-%m-%d_%H-%M-%S')
+datetime_tight_version=$(date '+%Y%m%d%H%M%S')
+date_version=$(date '+%Y-%m-%d')
+date_tight_version=$(date '+%Y%m%d')
+
+echo "data_general $data_general"
+echo "datetime_version $datetime_version"
+echo "datetime_tight_version $datetime_tight_version"
+echo "date_version $date_version"
+echo "date_tight_version $date_tight_version"
