0c659892e68a3daaedc4c539e163145e8adb504e
zhenjie zhu <devops@kubectl.net>
2024-04-29 11:38:43 +0800

    Add Node.js build commands to array_for.sh script
    An array of Node.js build commands has been added to the array_for.sh script. This will allow the script to install the npm packages from a custom registry and run the build command. Moreover, ShellCheck warnings have been suppressed for undefined variables.

--- .example/array_for.sh
+++ .example/array_for.sh
@@ -4,3 +4,14 @@ array=("A" "B" "C")
 for element in "${array[@]}"; do
   echo "$element"
 done
+
+echo "----"
+
+node_image="node:22"
+array=('npm install --registry=https://registry.npmmirror.com' 'npm run build')
+for element in "${array[@]}"; do
+  # shellcheck disable=SC2154
+  echo "bash <(curl -sSL https://code.kubectl.net/devops/build-project/raw/branch/main/node/build.sh)" \
+    -i "$node_image" \
+    -x "$element"
+done
