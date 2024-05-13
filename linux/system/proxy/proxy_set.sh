ec164a49bf67371086c74ebb340f6131cddcf435
zhenjie zhu <devops@kubectl.net>
2024-05-13 23:54:47 +0800

    Reorder functions in proxy_set script
    The functions in the proxy_set.sh script have been reordered. The function 'bashrc_remove_proxy' is now called before 'bashrc_config_proxy', ensuring that any previous proxy settings are removed before new ones are set. This order is essential for the smooth configuration of new proxy settings.

--- linux/system/proxy/proxy_set.sh
+++ linux/system/proxy/proxy_set.sh
@@ -13,7 +13,12 @@ Acquire::socks::Proxy "socks5h://127.0.0.1:1080";
 EOF
 }
 
+function bashrc_remove_proxy() {
+  sed -i '/^#4fe2411d-109a-4e92-82e8-9d612024b8e6/,/#c3f0e7a4-de15-4879-9845-01e53f3e8a2c/d' /root/.bashrc
+}
+
 function bashrc_config_proxy() {
+  bashrc_remove_proxy
   cat <<EOF >>/root/.bashrc
 #4fe2411d-109a-4e92-82e8-9d612024b8e6
 export ALL_PROXY=socks5h://127.0.0.1:1080
@@ -24,10 +29,6 @@ export HTTPS_PROXY=socks5h://127.0.0.1:1080
 alias wget='tsocks wget'
 #c3f0e7a4-de15-4879-9845-01e53f3e8a2c
 EOF
-}
-
-function bashrc_remove_proxy() {
-  sed -i '/^#4fe2411d-109a-4e92-82e8-9d612024b8e6/,/#c3f0e7a4-de15-4879-9845-01e53f3e8a2c/d' /root/.bashrc
 }
 
 action=$1
