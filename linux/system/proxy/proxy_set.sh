df7d518598b45a4f43b718f19f2ad86644b41761
zhenjie zhu <devops@kubectl.net>
2024-05-13 23:52:01 +0800

    Add script to manage proxy settings
    This commit introduces a new shell script, 'proxy_set.sh', which allows users to easily add or remove proxy configurations on both the .bashrc file and the apt package manager. The script supports both http and socks proxies and includes an option to install tsocks for wget commands.

---
+++ linux/system/proxy/proxy_set.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function apt_remove_proxy() {
+  rm -rf "/etc/apt/apt.conf.d/proxy.conf"
+}
+
+function apt_add_proxy() {
+  apt_remove_proxy
+  cat >"/etc/apt/apt.conf.d/proxy.conf" <<EOF
+Acquire::http::Proxy "socks5h://127.0.0.1:1080";
+Acquire::https::Proxy "socks5h://127.0.0.1:1080";
+Acquire::socks::Proxy "socks5h://127.0.0.1:1080";
+EOF
+}
+
+function bashrc_config_proxy() {
+  cat <<EOF >>/root/.bashrc
+#4fe2411d-109a-4e92-82e8-9d612024b8e6
+export ALL_PROXY=socks5h://127.0.0.1:1080
+export HTTP_PROXY=socks5h://127.0.0.1:1080
+export HTTPS_PROXY=socks5h://127.0.0.1:1080
+
+# apt install tsocks
+alias wget='tsocks wget'
+#c3f0e7a4-de15-4879-9845-01e53f3e8a2c
+EOF
+}
+
+function bashrc_remove_proxy() {
+  sed -i '/^#4fe2411d-109a-4e92-82e8-9d612024b8e6/,/#c3f0e7a4-de15-4879-9845-01e53f3e8a2c/d' /root/.bashrc
+}
+
+action=$1
+
+if [ "$action" == "add" ]; then
+  apt_add_proxy
+  bashrc_config_proxy
+else
+  apt_remove_proxy
+  bashrc_remove_proxy
+fi
+
+# export ALL_PROXY=socks5h://127.0.0.1:1080
+# export HTTP_PROXY=socks5h://127.0.0.1:1080
+# export HTTPS_PROXY=socks5h://127.0.0.1:1080
+# alias wget='tsocks wget'
