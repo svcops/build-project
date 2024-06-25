2115454e3c12a4d7fc767cd1839a5cfbfa0b5849
zhenjie zhu <tech@intellij.io>
2024-06-25 18:55:16 +0800

    🔧 Add color support to logging functions
    The modifications in log.sh introduces support for RED and GREEN colors in logging functions. Specifically, another two new functions, log_info and log_error have been added, to bring color-coded notifications in logging - improving the clarity and visibility of logs.

--- func/log.sh
+++ func/log.sh
@@ -1,3 +1,7 @@
+GREEN='\033[0;32m' # 绿色
+RED='\033[0;31m'   # 红色
+NC='\033[0m'       # reset
+
 function log() {
   local remark="$1"
   local msg="$2"
@@ -10,4 +14,32 @@ function log() {
   # shellcheck disable=SC2155
   local now=$(date +"%Y-%m-%d %H:%M:%S")
   echo -e "$now - [ $remark ] $msg"
+}
+
+function log_info() {
+  local remark="$1"
+  local msg="$2"
+  if [ -z "$remark" ]; then
+    remark="unknown remark"
+  fi
+  if [ -z "$msg" ]; then
+    msg="unknown message"
+  fi
+  # shellcheck disable=SC2155
+  local now=$(date +"%Y-%m-%d %H:%M:%S")
+  echo -e "${GREEN}$now - [ $remark ] $msg${NC}"
+}
+
+function log_error() {
+  local remark="$1"
+  local msg="$2"
+  if [ -z "$remark" ]; then
+    remark="unknown remark"
+  fi
+  if [ -z "$msg" ]; then
+    msg="unknown message"
+  fi
+  # shellcheck disable=SC2155
+  local now=$(date +"%Y-%m-%d %H:%M:%S")
+  echo -e "${RED}$now - [ $remark ] $msg${NC}"
 }
