0ec1a941827b69549fae6d4d539ce1c523ffb4b2
zhenjie zhu <tech@intellij.io>
2024-08-23 15:05:35 +0800

    🚀 Add scripts for managing various system proxies
    Introduced new scripts to handle proxy settings for wget, Docker, and APT. The `proxy_wget.sh` script is a placeholder. Docker and APT scripts now include add and remove proxy functions. Renamed `proxy_set.sh` to `proxy_bashrc.sh` and updated its functionality to focus solely on bashrc proxy settings.

--- linux/system/proxy/proxy_bashrc.sh
+++ linux/system/proxy/proxy_bashrc.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# remove proxy
-function apt_remove_proxy() {
-  rm -rf "/etc/apt/apt.conf.d/proxy.conf"
-}
-
-function apt_add_proxy() {
-  apt_remove_proxy
-  cat >"/etc/apt/apt.conf.d/proxy.conf" <<EOF
-Acquire::http::Proxy "socks5h://127.0.0.1:1080";
-Acquire::https::Proxy "socks5h://127.0.0.1:1080";
-Acquire::socks::Proxy "socks5h://127.0.0.1:1080";
-EOF
-}
-
 function bashrc_remove_proxy() {
   sed -i '/^#4fe2411d-109a-4e92-82e8-9d612024b8e6/,/#c3f0e7a4-de15-4879-9845-01e53f3e8a2c/d' /root/.bashrc
 }
@@ -35,10 +21,8 @@ EOF
 action=$1
 
 if [ "$action" == "add" ]; then
-  apt_add_proxy
   bashrc_config_proxy
 else
-  apt_remove_proxy
   bashrc_remove_proxy
 fi
 
