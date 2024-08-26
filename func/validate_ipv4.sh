d76b4932c94ec57f0c8b7e3408d303785dfbf075
zhenjie zhu <tech@intellij.io>
2024-08-26 18:32:50 +0800

    🛠️ Add IPv4 validation function
    Introduce a new function `validate_ipv4` to check if an IP address is valid according to a regular expression pattern. This function logs a message if the IP is valid or invalid and returns an error code on failure.

---
+++ func/validate_ipv4.sh
@@ -0,0 +1,11 @@
+function validate_ipv4() {
+  local ip=$1
+  local regex='^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+
+  if [[ $ip =~ $regex ]]; then
+    log_info "validate_ip" "Valid IP"
+  else
+    log_error "validate_ip" "Invalid IP"
+    return 1
+  fi
+}
