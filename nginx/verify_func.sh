bf58b1e6ed1e511c243ed77be27b582079599ff7
zhenjie zhu <tech@intellij.io>
2024-09-09 14:28:57 +0800

    🔧 Replace `log` with specific log levels
    Refactor logging in `verify_func.sh` to use `log_info`, `log_warn`, or `log_error` instead of generic `log` calls. This enhances log readability and clarity by differentiating between informational messages, warnings, and errors.

--- nginx/verify_func.sh
+++ nginx/verify_func.sh
@@ -7,15 +7,15 @@ source <(curl -SL $ROOT_URI/func/log.sh)
 source <(curl -SL $ROOT_URI/func/command_exists.sh)
 
 function verify_nginx_configuration() {
-  log "nginx" "Verify the nginx configuration file that docker-compose starts"
+  log_info "nginx" "Verify the nginx configuration file that docker-compose starts"
 
   local docker_in_docker="false"
   if ! command_exists docker; then
-    log "nginx" "docker command does not exits"
+    log_error "nginx" "docker command does not exits"
     return 1
   elif docker compose 2>&1 | grep -q "^docker: 'compose' is not a docker command."; then
-    log "nginx" "docker: 'compose' is not a docker command."
-    log "nginx" "use docker in docker"
+    log_warn "nginx" "docker: 'compose' is not a docker command."
+    log_warn "nginx" "use docker in docker"
     docker_in_docker="true"
   fi
 
@@ -23,42 +23,42 @@ function verify_nginx_configuration() {
   local service_name=$1
 
   if [ -z "$compose_file" ]; then
-    log "nginx" "compose file is empty, try use docker-compose.yml or docker-compose.yaml"
+    log_info "nginx" "compose file is empty, try use docker-compose.yml or docker-compose.yaml"
     if [ -f "docker-compose.yml" ]; then
       compose_file="docker-compose.yml"
     elif [ -f "docker-compose.yaml" ]; then
       compose_file="docker-compose.yaml"
     else
-      log "nginx" "cannot find docker-compose.yml or docker-compose.yaml in current directory"
+      log_error "nginx" "cannot find docker-compose.yml or docker-compose.yaml in current directory"
       return 1
     fi
   elif [ ! -f "$compose_file" ]; then
-    log "nginx" "compose file does not exits, [compose_file=$compose_file,service_name=$service_name] then return 1"
+    log_error "nginx" "compose file does not exits, [compose_file=$compose_file,service_name=$service_name] then return 1"
     return 1
   fi
 
   local COMPOSE_FILE_FOLDER
   COMPOSE_FILE_FOLDER=$(cd "$(dirname "$compose_file")" && pwd)
-  log "nginx" "compose file dir is $COMPOSE_FILE_FOLDER"
+  log_info "nginx" "compose file dir is $COMPOSE_FILE_FOLDER"
   local COMPOSE_FILE_NAME
   COMPOSE_FILE_NAME=$(basename "$compose_file")
 
   if [ -z "$service_name" ]; then
-    log "nginx" "service_name is empty,[compose_file=$compose_file,service_name=$service_name] then return 1"
+    log_error "nginx" "service_name is empty,[compose_file=$compose_file,service_name=$service_name] then return 1"
     return 1
   fi
 
   local output
   if command_exists docker-compose; then
-    log "nginx" "use docker-compose"
-    log "nginx" "$(docker-compose -f "$compose_file" run --rm -it "$service_name" nginx -v 2>&1 | tail -n 1)"
-    log "nginx" "docker-compose -f $compose_file run --rm -it $service_name nginx -t 2>&1 | tail -n 2 | grep 'nginx:'"
+    log_info "nginx" "use docker-compose"
+    log_info "nginx" "$(docker-compose -f "$compose_file" run --rm -it "$service_name" nginx -v 2>&1 | tail -n 1)"
+    log_info "nginx" "docker-compose -f $compose_file run --rm -it $service_name nginx -t 2>&1 | tail -n 2 | grep 'nginx:'"
     # 2>&1 重定向到标准输出
     output=$(docker-compose -f "$compose_file" run --rm -it "$service_name" nginx -t 2>&1 | tail -n 2 | grep 'nginx:')
   elif [ "true" == "$docker_in_docker" ]; then
-    log "nginx" "use docker compose plugin (docker in docker)"
+    log_info "nginx" "use docker compose plugin (docker in docker)"
 
-    log "nginx" "$(
+    log_info "nginx" "$(
       docker run --rm -it -v "/var/run/docker.sock:/var/run/docker.sock" \
         -v "$COMPOSE_FILE_FOLDER:$COMPOSE_FILE_FOLDER" \
         --privileged \
@@ -66,7 +66,7 @@ function verify_nginx_configuration() {
         docker compose -f "$COMPOSE_FILE_FOLDER/$COMPOSE_FILE_NAME" run --rm -it "$service_name" nginx -v 2>&1 | tail -n 1
     )"
     local compose_command="docker compose -f $COMPOSE_FILE_FOLDER/$COMPOSE_FILE_NAME run --rm -it $service_name nginx -t 2>&1 | tail -n 2 | grep 'nginx:'"
-    log "nginx" "\n  docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v $COMPOSE_FILE_FOLDER:$COMPOSE_FILE_FOLDER --privileged docker $compose_command"
+    log_info "nginx" "\n  docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v $COMPOSE_FILE_FOLDER:$COMPOSE_FILE_FOLDER --privileged docker $compose_command"
 
     output=$(
       docker run --rm -it --privileged \
@@ -77,18 +77,18 @@ function verify_nginx_configuration() {
     )
 
   else
-    log "nginx" "use docker compose plugin"
-    log "nginx" "$(docker compose -f "$compose_file" run --rm -it "$service_name" nginx -v 2>&1 | tail -n 1)"
-    log "nginx" "docker compose -f $compose_file run --rm -it $service_name nginx -t 2>&1 | tail -n 2 | grep 'nginx:'"
+    log_info "nginx" "use docker compose plugin"
+    log_info "nginx" "$(docker compose -f "$compose_file" run --rm -it "$service_name" nginx -v 2>&1 | tail -n 1)"
+    log_info "nginx" "docker compose -f $compose_file run --rm -it $service_name nginx -t 2>&1 | tail -n 2 | grep 'nginx:'"
     output=$(docker compose -f "$compose_file" run --rm -it "$service_name" nginx -t 2>&1 | tail -n 2 | grep 'nginx:')
   fi
 
   if [ -z "$output" ]; then
-    log "nginx" "output is empty. Unknown Configuration, [compose_file=$compose_file,service_name=$service_name] then return 1"
+    log_error "nginx" "output is empty. Unknown Configuration, [compose_file=$compose_file,service_name=$service_name] then return 1"
     return 1
   else
-    log "nginx" ">>> output <<<\n\n$output\n"
-    log "nginx" ">>> output <<<"
+    log_info "nginx" ">>> output <<<\n\n$output\n"
+    log_info "nginx" ">>> output <<<"
   fi
 
   # template
@@ -105,13 +105,13 @@ function verify_nginx_configuration() {
   status=$(echo "$output" | sed -n '2p')
 
   if echo "$status" | grep -q "successful"; then
-    log "nginx" "$status"
+    log_info "nginx" "$status"
     return 0
   elif echo "$status" | grep -q "failed"; then
-    log "nginx" "$reason"
+    log_error "nginx" "$reason"
     return 1
   else
-    log "nginx" "Unknown Configuration, [compose_file=$compose_file,service_name=$service_name] then return 1"
+    log_error "nginx" "Unknown Configuration, [compose_file=$compose_file,service_name=$service_name] then return 1"
     return 1
   fi
 
