c199b157b1162aba8e8fb05d6b1248f15346575d
zhenjie zhu <tech@intellij.io>
2024-10-05 14:58:48 +0800

    🛠️ Correct log message and reorder example tips
    Update the log message in the detect_os function for consistency. Also, reorder the example tips for clarity in displaying usage examples.

--- docker/install/install_apt.sh
+++ docker/install/install_apt.sh
@@ -18,7 +18,7 @@ function detect_os() {
   fi
 
   local os_name="$(. /etc/os-release && echo "$NAME")"
-  log "detect_os" "current system is $os_name"
+  log "detect_os" "current os is $os_name"
   if [[ $os_name =~ "Ubuntu" ]]; then
     OS="Ubuntu"
   elif [[ $os_name =~ "Debian" ]]; then
@@ -46,10 +46,10 @@ fi
 
 function tips() {
   log_info "tips" "SRC 为脚本的参数,源选择,可选 docker(官方源) tsinghua(清华源) aliyun(阿里云) intellij(镜像)"
-  log_info "tips" "e.g.: bash <(curl -SL $ROOT_URI/docker/install/install_apt.sh) intellij"
   log_info "tips" "e.g.: bash <(curl -SL $ROOT_URI/docker/install/install_apt.sh) tsinghua"
   log_info "tips" "e.g.: bash <(curl -SL $ROOT_URI/docker/install/install_apt.sh) aliyun"
   log_info "tips" "e.g.: bash <(curl -SL $ROOT_URI/docker/install/install_apt.sh) docker"
+  log_info "tips" "e.g.: bash <(curl -SL $ROOT_URI/docker/install/install_apt.sh) intellij"
 }
 
 SRC=$1
