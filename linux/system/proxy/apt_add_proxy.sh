21ac079285dc41c08c2268a549ad38ae8e2f6390
zhenjie zhu <tech@intellij.io>
2024-10-23 21:37:03 +0800

    🔧 Fix directory existence check in apt_add_proxy.sh
    Changed the script to check for the existence of the /etc/apt/ directory instead of /etc/apt/apt.conf.d/. This ensures that the script fails appropriately if the main apt directory is missing.

--- linux/system/proxy/apt_add_proxy.sh
+++ linux/system/proxy/apt_add_proxy.sh
@@ -4,8 +4,8 @@ source <(curl -SL https://gitlab.com/iprt/shell-basic/-/raw/main/build-project/b
 source <(curl -SL $ROOT_URI/func/log.sh)
 source <(curl -SL $ROOT_URI/func/date.sh)
 
-if [ ! -d "/etc/apt/apt.conf.d/" ]; then
-  log_error "proxy_apt occurred error: directory /etc/apt/apt.conf.d/ does not exist"
+if [ ! -d "/etc/apt/" ]; then
+  log_error "proxy_apt occurred error: directory /etc/apt/ does not exist"
   exit
 fi
 
