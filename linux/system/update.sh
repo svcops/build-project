97004b45f112249933024c6810e7e9f0281f6be8
zhenjie zhu <tech@intellij.io>
2024-10-29 20:18:03 +0800

    🛠️ Comment out auto library restart setting
    Disable automatic library restart prompt in `update.sh` script. Added additional comments to clarify the non-interactive and restart modes for future reference and maintainability.

--- linux/system/update.sh
+++ linux/system/update.sh
@@ -12,8 +12,17 @@ function apt_upgrade() {
   apt-get update -y
   # WARNING: apt does not have a stable CLI interface. Use with caution in scripts
   #  DEBCONF_NONINTERACTIVE_SEEN=true \
-  echo '* libraries/restart-without-asking boolean true' | debconf-set-selections
-  # Upgrade packages with automatic service restart NEEDRESTART_MODE=a
+  # echo '* libraries/restart-without-asking boolean true' | debconf-set-selections
+
+  #  DEBIAN_FRONTEND=noninteractive：设置包管理器在非交互模式下运行，从而避免在安装或升级软件包时弹出交互式对话框。
+  #  DEBCONF_NONINTERACTIVE_SEEN=true：确保所有的 debconf 配置项都已经标记为“Seen”（已处理），使其不会在后续的操作中再提示。
+
+  #  NEEDRESTART_MODE=a
+  # a: 自动重启所有需要重启的服务。
+  # i: 显示交互提示，询问用户是否需要重启服务（默认行为）
+  # l: 只列出需要重启的服务，不实际执行重启。
+  # m: 像 i 一样，但在没有终端可用时使用自动方式。
+  # n: 不重启任何服务。
   DEBIAN_FRONTEND=noninteractive \
     DEBCONF_NONINTERACTIVE_SEEN=true \
     NEEDRESTART_MODE=a \
