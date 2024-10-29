78c16b1dd2d6e4158461389dea2938b576ffa33e
zhenjie zhu <tech@intellij.io>
2024-10-29 19:41:17 +0800

    🗑️ Add script to remove Snap from Ubuntu
    Introduce `rm_snap.sh` script to completely remove Snap and its related directories. Integrated `rm_snap.sh` into Debian `system_init.sh` for automatic Snap removal during initialization.

--- linux/system/debian/system_init.sh
+++ linux/system/debian/system_init.sh
@@ -10,3 +10,7 @@ bash <(curl -sSL $ROOT_URI/linux/system/update.sh)
 log_info "init" "init bashrc ls"
 
 bash <(curl -sSL $ROOT_URI/linux/system/bashrc/config_bashrc_ls.sh)
+
+log_info "init" "remove snap"
+
+bash <(curl -sSL $ROOT_URI/linux/system/ubuntu/rm_snap.sh)
---
+++ linux/system/ubuntu/rm_snap.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# shellcheck disable=SC2164 disable=SC2086 disable=SC1090
+source <(curl -SL https://gitlab.com/iprt/shell-basic/-/raw/main/build-project/basic.sh)
+source <(curl -sSL $ROOT_URI/func/log.sh)
+source <(curl -sSL $ROOT_URI/func/command_exists.sh)
+
+if ! command_exists snap; then
+  log_info "prepare" "command snap does not exist"
+  exit
+fi
+
+function remove_snap() {
+
+  function remove_snap_software() {
+    for i in {1..3}; do
+      log_info "remove_snap_software" "times $i"
+      for p in $(snap list | awk '{print $1}'); do
+        sudo snap remove $p
+      done
+    done
+  }
+
+  function remove_snap_core() {
+    sudo systemctl stop snapd
+    sudo systemctl disable --now snapd.socket
+    for m in /snap/core/*; do
+      sudo umount $m
+    done
+  }
+
+  function remove_snap_apt() {
+    sudo apt-get autoremove -y --purge snapd
+  }
+
+  function clear_snap_directory() {
+    rm -rf ~/snap
+    sudo rm -rf /snap
+    sudo rm -rf /var/snap
+    sudo rm -rf /var/lib/snapd
+    sudo rm -rf /var/cache/snapd
+  }
+
+  function reject_apt_install() {
+    sudo sh -c "cat > /etc/apt/preferences.d/no-snapd.pref" <<EOL
+Package: snapd
+Pin: release a=*
+Pin-Priority: -10
+EOL
+  }
+
+  function validate() {
+    apt-get install snapd -y
+  }
+
+  remove_snap_software
+  remove_snap_core
+  remove_snap_apt
+  clear_snap_directory
+  reject_apt_install
+  validate
+}
+
+remove_snap
