fcb0f0f0753631a2ac3685a9e61929e717bf2475
zhenjie zhu <tech@intellij.io>
2024-11-14 01:13:43 +0800

    fix: remote SHELL_FOLDER

--- devops/es/prepare_compose.sh
+++ devops/es/prepare_compose.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # shellcheck disable=SC1090 disable=SC2086 disable=SC2155 disable=SC2128 disable=SC2028 disable=SC2164 disable=SC2162
-SHELL_FOLDER=$(cd "$(dirname "$0")" && pwd) && cd "$SHELL_FOLDER"
 [ -z $ROOT_URI ] && source <(curl -sSL https://gitlab.com/iprt/shell-basic/-/raw/main/build-project/basic.sh)
 echo -e "\033[0;32mROOT_URI=$ROOT_URI\033[0m"
 # ROOT_URI=https://dev.kubectl.net
@@ -62,7 +61,7 @@ log_warn "elasticsearch" "current_dir=$current_dir"
 log_warn "elasticsearch" "current_dir=$current_dir"
 log_warn "elasticsearch" "current_dir=$current_dir"
 
-read -p "Confirm prepare es's files in $current_dir" answer
+read -p "Confirm prepare es's files in $current_dir [y/n]" answer
 
 if [ $answer == "y" ]; then
   answer=""
