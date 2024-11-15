b8a9bdaee1299cf3e96f578c41d72c9de06de252
zhenjie zhu <tech@intellij.io>
2024-11-16 04:01:47 +0800

    🔧 Add script to set Elasticsearch user passwords
    Introduce a new script `set-passwords.sh` to set passwords for Elasticsearch users. This script ensures the specified Elasticsearch user exists and interacts with the `elasticsearch-setup-passwords` utility to set passwords for built-in users.

---
+++ devops/es/system/set-passwords.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# shellcheck disable=SC1090 disable=SC2086 disable=SC2155 disable=SC2128 disable=SC2028 disable=SC2086 disable=SC2162 disable=SC2117
+[ -z $ROOT_URI ] && source <(curl -sSL https://gitlab.com/iprt/shell-basic/-/raw/main/build-project/basic.sh)
+echo -e "\033[0;32mROOT_URI=$ROOT_URI\033[0m"
+# ROOT_URI=https://dev.kubectl.net
+
+source <(curl -SL $ROOT_URI/func/log.sh)
+
+read -p "input es user: (default is es) :" es_user
+# shellcheck disable=SC2086
+if [ -z $es_user ]; then
+  es_user="es"
+fi
+
+if ! id $es_user &>/dev/null; then
+  log_error "elasticsearch" "user $es_user is not exist"
+  exit 1
+fi
+
+log_info "elasticsearch" "es_user=$es_user"
+
+if [ ! -f "bin/elasticsearch-setup-passwords" ]; then
+  log_error "elasticsearch" "bin/elasticsearch-setup-passwords is not exist"
+  exit 1
+else
+  log_info "elasticsearch" "bin/elasticsearch-setup-passwords is exist"
+
+  log_info "elasticsearch" "su $es_user"
+  su $es_user
+  read -p "input es password: " es_password
+  if [ -z $es_password ]; then
+    log_error "elasticsearch" "es_password is empty"
+    exit 1
+  fi
+  # Set passwords for built-in users
+  echo -e "y\n$es_password\n$es_password\n$es_password\n$es_password\n$es_password\n$es_password\n$es_password\n$es_password\n$es_password\n$es_password\n$es_password\n$es_password\n" |
+    bin/elasticsearch-setup-passwords interactive
+  exit 0
+
+fi
