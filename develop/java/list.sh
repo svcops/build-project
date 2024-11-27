4224cedf8019aa44d48b27a8148a9620528f911a
zhenjie zhu <tech@intellij.io>
2024-11-28 01:26:53 +0800

    ✨ Use silent mode in curl commands
    Modified curl commands to use the silent mode (`-sSL`) for sourcing scripts. This prevents unnecessary progress and error messages from being displayed, enhancing script output cleanliness.

--- develop/java/list.sh
+++ develop/java/list.sh
@@ -4,8 +4,8 @@
 echo -e "\033[0;32mROOT_URI=$ROOT_URI\033[0m"
 # ROOT_URI=https://dev.kubectl.net
 
-source <(curl -SL $ROOT_URI/func/log.sh)
-source <(curl -SL $ROOT_URI/func/command_exists.sh)
+source <(curl -sSL $ROOT_URI/func/log.sh)
+source <(curl -sSL $ROOT_URI/func/command_exists.sh)
 
 if ! command_exists jps; then
   log_error "prepare" "command jps does not exist"
