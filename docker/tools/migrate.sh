1afb11e8f18d808327177cbf5c52bc0aef7b1aa0
zhenjie zhu <tech@intellij.io>
2025-03-28 15:53:09 +0800

    Refactor image pull logic in migration script
    Refactored the image pulling logic to include a confirmation prompt when the image already exists. Introduced helper functions for checking image existence and performing the pull operation for better code organization and readability.

--- docker/tools/migrate.sh
+++ docker/tools/migrate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC1090 disable=SC2086 disable=SC2155 disable=SC2128 disable=SC2028 disable=SC2181 disable=SC2046
+# shellcheck disable=SC1090 disable=SC2086 disable=SC2155 disable=SC2128 disable=SC2028 disable=SC2181 disable=SC2046 disable=SC2162
 [ -z $ROOT_URI ] && source <(curl -sSL https://gitlab.com/iprt/shell-basic/-/raw/main/build-project/basic.sh)
 echo -e "\033[0;32mROOT_URI=$ROOT_URI\033[0m"
 # ROOT_URI=https://dev.kubectl.net
@@ -26,17 +26,38 @@ if [ -z "$to_image" ]; then
   exit 1
 fi
 
-docker pull "$from_image"
-if [ $? -ne 0 ]; then
-  log_error "migrate" "docker pull $from_image failed"
-  exit 1
+function image_exists() {
+  local image_name_tag="$1"
+  if [ $(docker image ls $image_name_tag | wc -l) -ge 2 ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function docker_pull() {
+  local image_name_tag="$1"
+  docker pull $image_name_tag
+  if [ $? -ne 0 ]; then
+    log_error "migrate" "docker pull $from_image failed"
+    exit 1
+  fi
+}
+
+# 如果镜像存在，判断是否要再次pull
+if image_exists $from_image; then
+  # 判断是否要再次pull
+  read -p "Image $from_image already exists, do you want to pull it again?[default: y] (y/n) :" answer
+  if [ -z "$answer" ]; then
+    answer="y"
+  fi
+  if [ "$answer" == "y" ]; then
+    docker_pull $from_image
+  fi
 fi
 
-if [ $(docker image ls $from_image | wc -l) -ge 2 ]; then
-  log_info "migrate" "docker pull $from_image success"
-else
-  log_error "migrate" "docker pull $from_image failed"
-  exit 1
+# 再次判断镜像是否存在
+if image_exists $from_image; then
+  docker tag "$from_image" "$to_image"
+  docker push "$to_image"
 fi
-docker tag "$from_image" "$to_image"
-docker push "$to_image"
