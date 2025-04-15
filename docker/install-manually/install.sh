f517510de41d91a37b9c4d24eb8d80e38d3716d5
zhenjie zhu <tech@intellij.io>
2025-04-15 14:28:14 +0800

    ✨ Simplify ROOT_URI initialization in shell scripts
    Removed redundant echo statements during ROOT_URI initialization across multiple scripts. This ensures cleaner and more concise code, improving readability without altering functionality.

--- docker/install-manually/install.sh
+++ docker/install-manually/install.sh
@@ -3,8 +3,8 @@
 SHELL_FOLDER=$(cd "$(dirname "$0")" && pwd)
 cd "$SHELL_FOLDER"
 
-[ -z $ROOT_URI ] && source <(curl -sSL https://gitlab.com/iprt/shell-basic/-/raw/main/build-project/basic.sh)
-echo -e "\033[0;32mROOT_URI=$ROOT_URI\033[0m" && export ROOT_URI=$ROOT_URI
+[ -z $ROOT_URI ] && source <(curl -sSL https://gitlab.com/iprt/shell-basic/-/raw/main/build-project/basic.sh) && export ROOT_URI=$ROOT_URI
+
 source <(curl -sSL $ROOT_URI/func/log.sh)
 
 arch=$1
--- linux/system/update.sh
+++ linux/system/update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # shellcheck disable=SC1090 disable=SC2154  disable=SC2086 disable=SC2028
-[ -z $ROOT_URI ] && source <(curl -sSL https://gitlab.com/iprt/shell-basic/-/raw/main/build-project/basic.sh)
-echo -e "\033[0;32mROOT_URI=$ROOT_URI\033[0m" && export ROOT_URI=$ROOT_URI
+[ -z $ROOT_URI ] && source <(curl -sSL https://gitlab.com/iprt/shell-basic/-/raw/main/build-project/basic.sh) && export ROOT_URI=$ROOT_URI
 # ROOT_URI=https://dev.kubectl.net
 
 source <(curl -sSL $ROOT_URI/func/log.sh)
