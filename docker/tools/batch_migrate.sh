f5b32b8a5480bdc9ad2624c98563d36ac7dd77b6
zhenjie zhu <tech@intellij.io>
2025-09-24 10:10:36 +0800

    Refactor batch_migrate.sh: update ROOT_URI handling, add platform-specific logic for Windows, and improve shellcheck directive coverage.

--- docker/tools/batch_migrate.sh
+++ docker/tools/batch_migrate.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-# shellcheck disable=SC1090
-export ROOT_URI="https://gitlab.com/svcops/build-project/-/raw/main"
-source <(curl -sSL "$ROOT_URI/func/log.sh")
+# shellcheck disable=SC2164,SC1090,SC2086
+[ -z $ROOT_URI ] && source <(curl -sSL https://gitlab.com/iprt/shell-basic/-/raw/main/build-project/basic.sh)
+echo -e "\033[0;32mROOT_URI=$ROOT_URI\033[0m"
+# export ROOT_URI=https://dev.kubectl.net
+
+source <(curl -sSL $ROOT_URI/func/log.sh)
+source <(curl -sSL $ROOT_URI/func/ostype.sh)
+
+if is_windows; then
+  log_info "build" "build in windows"
+  export MSYS_NO_PATHCONV=1
+fi
 
 declare -g config_json="$1"
 declare -g test_mode=${2:-false}
