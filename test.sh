6c420997bd3ddb293eca4d58c7c6902a81abb08b
zhenjie zhu <tech@intellij.io>
2025-09-25 20:53:56 +0800

    Refactor test.sh: remove redundant `export ROOT_URI` statement for improved clarity and streamlined logic.

--- test.sh
+++ test.sh
@@ -5,7 +5,6 @@
 set -euo pipefail
 
 # 初始化根URI和依赖
-[ -z "${ROOT_URI:-}" ] && source <(curl -sSL https://gitlab.com/iprt/shell-basic/-/raw/main/build-project/basic.sh) &&
-  export ROOT_URI=$ROOT_URI
+[ -z "${ROOT_URI:-}" ] && source <(curl -sSL https://gitlab.com/iprt/shell-basic/-/raw/main/build-project/basic.sh)
 
 echo "ROOT_URI: $ROOT_URI"
