3fa68bae3d7ff06e84401c45dd09c6dc1482e461
zhenjie zhu <dev@intellij.io>
2026-08-14 20:20:01 +0800

    Add `all_commands_exist` function to check multiple commands' existence
    - Introduced `all_commands_exist` to validate the presence of multiple commands at once.
    - Utilizes the existing `command_exists` function for individual checks.

--- func/command_exists.sh
+++ func/command_exists.sh
@@ -1,3 +1,12 @@
 function command_exists() {
   type "$1" &>/dev/null
 }
+
+function all_commands_exist() {
+  for cmd in "$@"; do
+    if ! command_exists "$cmd"; then
+      return 1
+    fi
+  done
+  return 0
+}
